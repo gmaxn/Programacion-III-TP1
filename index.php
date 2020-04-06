380c1daf55974e858bd63de4a6b342c67c426fb6
gmaxn <maxi.ng@hotmail.com>
2020-04-05 23:41:38 -0300

    added ErrorHandlingMiddleWare

--- index.php
+++ index.php
@@ -8,6 +8,8 @@ require __DIR__ . './vendor/autoload.php';
 $app = AppFactory::create();
 $app->setBasePath('/TP1');
 
+$app->addErrorMiddleware(true, true, false);
+
 $app->get('/hello-world', function (Request $request, Response $response, $args) {
 
     $response->getBody()->write("Hello world!");  
